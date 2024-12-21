d45e42da516d489df9dff5b60f9ad5d4ed98e41b
Олег <olegkovalets1112003@knu.ua>
2024-12-21 20:51:12 +0200

    Updates

---
+++ assets/index-D-Px2ru0.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DSzhwYcP.js";const n=()=>o.jsx("div",{children:"PokemonInfo page"});export{n as default};
---
+++ assets/index-DA2YoS8U.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DSzhwYcP.js";const r=()=>e.jsx("div",{children:"PokemonFinder page"});export{r as default};
